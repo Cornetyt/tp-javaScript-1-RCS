f76d005f1817f07d30dcd2bd769e33e200cef30d
Diego <diegocornet2020rd@gmail.com>
2023-10-29 22:35:50 -0300

    Tarea de trello numero 3

---
+++ js/trello-ejercicio-3.js
@@ -0,0 +1,8 @@
+let gradosCelsius = 37.5;
+let mensaje = gradosCelsius + " °C";
+let gradosFahrenheit = (gradosCelsius * 9/5) + 32;
+let mensajeF = gradosFahrenheit + " °F"
+
+console.log(mensaje)
+console.log(mensajeF)
+console.log(mensaje+ " equivale a "+ mensajeF)
