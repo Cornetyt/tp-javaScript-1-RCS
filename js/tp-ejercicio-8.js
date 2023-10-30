34a4421266089d7ce903c7d5863af3c8c6e2d736
Diego <diegocornet2020rd@gmail.com>
2023-10-30 00:21:46 -0300

    Tarea del tp ejercicio 8

---
+++ js/tp-ejercicio-8.js
@@ -0,0 +1,7 @@
+let numeroUno = parseInt(prompt("ingrese su número"));
+
+if (numeroUno % 2 == 0) {
+  alert("el " + numeroUno + " es divisible por 2");
+} else {
+  alert("el " + numeroUno + " no es divisible por 2");
+}
