fab1757f98306b3e1560997d51ccd091cf4a5a5f
Diego <diegocornet2020rd@gmail.com>
2023-10-29 23:24:21 -0300

    Tarea del tp ejercicio 5

---
+++ js/tp-ejercicio-5.js
@@ -0,0 +1,3 @@
+let numeroUno = parseInt(prompt("Ingrese el primer número"));
+let numeroDos = parseInt(prompt("Ingrese el segundo número"));
+alert("El resultado final de la suma es " + (numeroUno + numeroDos));
